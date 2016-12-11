44d02f7a0c678f4e592d0c96ec42bce854f61df1
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-12-11 17:21:21 -0500

    Changes fields in Node.java to relevant Tree references

--- Node.java
+++ Node.java
@@ -1,35 +1,35 @@
 /* Node.java
  * Written by Vincent Zelinsky
  * 
- * Contains the necessary constructors, getters, and setters to create a Node and interact with List.java
+ * Contains the necessary constructors, getters, and setters to create a Node and interact with Tree.java
  */
 public class Node {
   //Private fields
   //Contains the String stored in a Node
   private String data;
-  //Contains a reference to the next Node object
-  private Node next;
-  //Contains a reference to the prev Node object
-  private Node prev;
+  //Contains a reference to the left child Node object
+  private Node left;
+  //Contains a reference to the right child Node object
+  private Node right;
   
   //Constructors
   //Accepts a String argument
   public Node(String dataInput) {
     //Sets data to the argument passed, a String
     this.data = dataInput;
-    //Sets next to null
-    this.next = null;
-    //Sets prev to null
-    this.prev = null;
+    //Sets left to null
+    this.left = null;
+    //Sets right to null
+    this.right = null;
   }
-  //Accepts arguments for the fields data, next, and prev
-  public Node(String dataInput, Node n, Node p) {
+  //Accepts arguments for the fields data, left, and right
+  public Node(String dataInput, Node l, Node r) {
     //Sets data to the first argument passed, a String
     this.data = dataInput;
-    //Sets next to the second argument passed, a reference to a Node object
-    this.next = n;
-    //Sets prev to the third argument passed, a reference to a Node object
-    this.prev = p;
+    //Sets left to the second argument passed, a reference to a Node object
+    this.left = l;
+    //Sets right to the third argument passed, a reference to a Node object
+    this.right = r;
   }
   
   //Getter methods
@@ -37,25 +37,25 @@ public class Node {
   public String getData(){
     return data;
   }
-  //Returns the reference to the Node stored at next
-  public Node getNext(){
-    return next;
+  //Returns the reference to the Node stored at left
+  public Node getLeft(){
+    return left;
   }
-  //Returns the reference to the Node stored at prev
-  public Node getPrev(){
-    return prev;
+  //Returns the reference to the Node stored at right
+  public Node getRight(){
+    return right;
   }
   //Setter methods
   //Sets the data field to the argument passed
   public void setData(String dataInput){
     this.data = dataInput;
   }
-  //Sets the next Node to the referenced Node in the argument
-  public void setNext(Node nextInput){
-    this.next = nextInput;
+  //Sets the right Node to the referenced Node in the argument
+  public void setRight(Node rightInput){
+    this.right = rightInput;
   }
-  //Sets the prev Node to the referenced Node in the argument
-  public void setPrev(Node prevInput){
-    this.prev = prevInput;
+  //Sets the left Node to the referenced Node in the argument
+  public void setLeft(Node leftInput){
+    this.left = leftInput;
   }
 }
