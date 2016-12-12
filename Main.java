682425e29de52dbc7352ff65bb2992bbb586ec32
LAPTOP-L6JQBG9C\Vinny <vzelinsky@albany.edu>
2016-12-11 19:53:33 -0500

    Tests new methods which work as intended

--- Main.java
+++ Main.java
@@ -5,5 +5,17 @@
  */
 public class Main {
   public static void main(String[] args) {
+    Tree myTree = new Tree();
+    myTree.insertNode("Sarah");
+    myTree.insertNode("Bart");
+    myTree.insertNode("Al");
+    myTree.insertNode("Zach");
+    myTree.insertNode("Alberto");
+    myTree.insertNode("Alberto");
+    myTree.insertNode("Alberto");
+    myTree.insertNode("Alberto");
+    myTree.insertNode("Sarah");
+    myTree.printInOrder();
+    System.out.println(myTree.getRoot().getCount());
   }
 }
